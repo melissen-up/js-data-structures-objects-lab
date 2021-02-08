d8b66e00d4b9ec3cc31b7455958eb3728893b0a3
Melissa Faulner <melissa.faulner@gmail.com>
2021-02-08 11:34:39 -0500

    commit tree 19c23e496ccc367ab64702e62d8f36159cd94647

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver};
+
+    newDriver[key] = value;
+
+    return newDriver;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const anotherNewDriver = {...driver};
+
+    delete anotherNewDriver[key];
+
+    return anotherNewDriver;
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+
+    return driver;
+};
